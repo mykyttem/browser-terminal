f456cc1218470cfe132fb6031c47cd8189bfd5ac
mykyttem <nrabicenko5@gmail.com>
2024-11-10 23:46:14 +0200

    Updated client: added new tab 'TaskList'

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import React, { useState } from 'react';
 import BodyTerminal from './cmd/BodyTerminal';
 import Tabs from './components/tabs/Tabs';
 import Settings from './components/Settings';
+import TaskList from './components/TaskList';
 import './styles/App.css';
 
 function App() {
   const [activeTab, setActiveTab] = useState('Terminal');
   const [theme, setTheme] = useState('dark');
-  const tabs = ['Terminal', 'Settings', 'Documentation', 'More'];
+  const tabs = ['Terminal', 'Tasks', 'Settings', 'Documentation', 'More'];
 
   return (
     <div className={`App ${theme}`}>
       <Tabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
       {activeTab === 'Terminal' ? (
         <BodyTerminal theme={theme} />
+      ) : activeTab === 'Tasks' ? (
+        <TaskList setTheme={setTheme}/>
       ) : (
         <Settings setTheme={setTheme} />
       )}
